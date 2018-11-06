d6a4faa2167fbe18fa12d18963f9c4231ac01642
Shirley Wang <shirleywang527@gmail.com>
2018-11-06 10:12:27 +0100

    script-del2.js is done

--- script-del2.js
+++ script-del2.js
@@ -134,16 +134,16 @@
 //     console.log(x + '\t' + (x / 2).toFixed(2) + '\t' + (x / 3).toFixed(2));
 // }
 
-//s.18
-const favFoods = ['Ärtsoppa', 'Röd Curry', 'Spaghetti Bolognese', 'Pizza Hawaii', 'BBQ', 'Dumplings', 'Tempura'];
-const meal = Math.floor(Math.random() * favFoods.length);
-const day = Math.floor(Math.random() * 6) + 1;
+// //s.18
+// const favFoods = ['Ärtsoppa', 'Röd Curry', 'Spaghetti Bolognese', 'Pizza Hawaii', 'BBQ', 'Dumplings', 'Tempura'];
+// const meal = Math.floor(Math.random() * favFoods.length);
+// const day = Math.floor(Math.random() * 6) + 1;
 
-if(day == 5) {
-    document.write('Du ska äta ute idag!');
-}else{
-    document.write(`Dagens maträtt är ${favFoods[meal]}.`);
-}
+// if(day == 5) {
+//     document.write('Du ska äta ute idag!');
+// }else{
+//     document.write(`Dagens maträtt är ${favFoods[meal]}.`);
+// }
 
 // //s.19
 // let lön = 500;
@@ -195,7 +195,203 @@ if(day == 5) {
 // }
 // console.log(average());
 
+// //s.22
+// let sheep = 1;
+// for(let monthes = 1; monthes <= 12; monthes++)
+//     sheep *= 4;
+// console.log(sheep);
 
+// //s.23
+// function arraySum(arr) {
+//     if(arr === 0) return 0;
+//     else return arr + arraySum(--arr);
+// }
+// console.log(arraySum(4));
+
+// //s.24
+// function arraySum2D(arr2d) {
+//     const result = arr2d.reduce( (accumulator, currentValue) => accumulator.concat(currentValue), [])   //to flatten the 2d array
+//         .reduce( (accumulator, currentValue) => accumulator + currentValue, 0); //to sum up all values
+
+//     return result;
+// }
+
+// console.log(arraySum2D([[1,2,3],[4,5,6]]));
+
+// //s.25
+// const n = parseInt(prompt('Skriv ett heltal: '));
+// function summa(n) {
+//     if(Number.isInteger(n)) {
+//         // console.log('yep');
+//         if(n === 0) return 0;
+//         else if(n < 0) return -1;
+//         return n + summa(--n);
+//     }else {
+//         alert('Fel! Skriv ett nummer!');
+//     }
+// }
+// summa(n);
+
+// // s.26
+// function squareSum() {
+//     const input = prompt('Ange två tal med en , däremellan');
+//     const arr = input.split(',');
+
+//     for(let i = 0; i < arr.length; i++) {
+//         return arr[0] * arr[0] + arr[1] * arr[1];
+//     }
+// }
+// console.log(squareSum());
+
+// //s. 27
+// for(let i = 1; i <= 100; i++) {
+//     let res = '';
+//     if(i % 3 === 0) res += 'Fizz';
+//     if(i % 5 === 0) res += 'Buzz';
+
+//     console.log(res || i);
+// }
+// /* alternatively */
+// for(let j = 1; j <= 100; j++) {
+//     let output = '';
+//     if(j % 3 === 0) output += 'Fizz';
+//     if(j % 5 === 0) output += 'Buzz';
+//     if(output === '') output = j;
+
+//     console.log(output);
+// }
+
+// //s. 28
+// const interestRate = parseInt(prompt('Ange räntesatsen i procent: '));
+// const totalYears = parseInt(prompt('Antal år man vill gärna spara pengarna: '));
+// let capital = 1000;
+
+// document.write('Antal År' + ' | ' + 'Kapital' + '<hr>');
+
+// for(let i = 1; i <= totalYears; i++) {
+//     capital = (capital * (1 + interestRate / 100)).toFixed(2);
+//     console.log(i + '. ' + capital);
+//     document.write(i + ' år' + ' | ' + capital + ' kr' + '<br>');
+// }
+
+//s. 30
+const h = document.getElementById('h');
+h.innerHTML = h.innerHTML.toUpperCase();
+h.style.color = 'maroon';
+
+//s. 31
+const h2 = document.getElementsByTagName('h2');
+h2[0].className = 'nice';
+
+// //s. 32
+// const p = document.getElementsByTagName('p');
+
+// for(let i = 0; i < p.length; i++) {
+//     if(p[i].innerHTML.endsWith('.') != true){
+//         p[i].innerHTML += '.';
+//     }
+// }
+
+//s. 33
+const p2 = document.querySelectorAll('p');
+
+for(let i = 0; i < p2.length; i++) {
+    let str = p2[i].innerHTML;
+    str = str.charAt(0).toUpperCase() + str.substring(1);
+    p2[i].innerHTML = str;
+    console.log(str);
+}
+
+// //s. 35
+// function showMsg() {
+//     alert('Welcome!');
+// }
+// window.addEventListener('load', showMsg);
+
+//s. 36
+const btn = document.getElementById('btn');
+function bgColor() {
+    document.body.bgColor = 'red';
+}
+btn.addEventListener('click', bgColor);
+
+//s. 37
+function myFunction() {
+    document.getElementById('demo').innerHTML = 'Hello World!';
+}
+
+const btn1 = document.getElementById('btn1');
+btn1.addEventListener('click', myFunction);
+
+//s.38
+const btn2 = document.getElementById('btn2');
+function hideTexts() {
+    // document.getElementById('demo1').innerHTML = '';
+    document.getElementById('demo1').style.display = 'none';
+}
+btn2.addEventListener('click', hideTexts);
+
+//s.39
+const txt = document.getElementById('txt');
+function hideTexts() {
+    // txt.style.display = 'none';
+    txt.innerHTML = '';
+}
+// txt.addEventListener('click', hideTexts);
+txt.onclick = hideTexts;
+
+//s. 40
+const txt1 = document.getElementById('txt1');
+function txtColor() {
+    // document.getElementById('txt1').style.color = 'green';
+    txt1.style.color = 'green';
+}
+txt1.addEventListener('mousemove', txtColor);
+
+//s. 41
+const btn3 = document.getElementById('btn3');
+function getTime() {
+    document.getElementById('time').innerHTML = new Date().toTimeString().substring(0, 8);
+}
+btn3.addEventListener('click', getTime);
+
+//s. 42
+/*
+The script indicates that when the button element with an id of 'btn' is clicked it grabs the element and fires up an event listener which triggers a display() function. This function shows the date string up in the element with the id 'demo'.
+*/
+
+//s. 43
+/*
+When Start button is clicked, a timer runs that will add one * per millisecond as the functin 'count' dictates. As soon as Stop button is clicked, the function stops running. No astrid will be added onto the document.
+*/
+
+
+
+// let row;
+// let col;
+
+// function createTable(row, col) {
+//     let table = document.createElement('table');
+//     table.setAttribute('border', '1');
+//     //as long as row is less than totalYears keep looping
+//     for(let i = 0; i < row; i++) {
+//         //a detached <tr>
+//         let tRow = document.createElement('tr');
+//         //inner loop
+//         for(let j = 0; j < col; j++) {
+//             //a detached <td>
+//             let tData = document.createElement('td');
+//             tData.textContent = capital;
+//             //append tData as many times as cols
+//             tRow.appendChild(tData);
+//         }
+//         //just like how tData added to row add tRow to table
+//         table.appendChild(tRow);
+//     }
+//     document.body.appendChild(table);
+// }
+
+// createTable(4, 2);
 
 
 
